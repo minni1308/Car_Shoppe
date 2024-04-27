7b181640d030d201be55284b0deab32185f345f2
minni1308 <47415221+minni1308@users.noreply.github.com>
2024-04-27 12:34:27 -0400

    Add files via upload

---
+++ fronend/src/actions/Loan.js
@@ -0,0 +1,13 @@
+export const appliedLoans = (loans) => ({
+    type:'APPLIED_LOANS',
+    
+    loans
+    // customerId:customerId,
+    // loanAmount:loanAmount,
+    // emi:emi,
+    // carCost:carCost,
+    // selectedFile:selectedFile,
+    // time:time,
+    // carName:carName,
+    // refId:refId
+})
